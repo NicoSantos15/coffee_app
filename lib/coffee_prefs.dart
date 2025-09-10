ac8d9d9e9b6b34bf59404d0584b373e3bc4a9446
Nico Santos <nicodiazsantos@gmail.com>
2025-09-10 16:21:57 +0930

    initial commit

---
+++ lib/coffee_prefs.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'styled_body_text.dart';
+import 'styled_button.dart';
+
+class CoffeePrefs extends StatefulWidget {
+  const CoffeePrefs({super.key});
+
+  @override
+  State<CoffeePrefs> createState() => _CoffeePrefsState();
+}
+
+class _CoffeePrefsState extends State<CoffeePrefs> {
+
+  int strength = 1;
+  int sugars = 1;
+
+  void increaseStrength() {
+    setState(() {
+      strength = strength < 5 ? strength + 1 : 1;
+    });
+  }
+  void increaseSugars() {
+    setState(() {
+      sugars = sugars < 5 ? sugars + 1 : 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            const StyledBodyText('Coffee Shots: '),
+
+            for (int i = 0; i < strength; i++) 
+              Image.asset('assets/img/coffee_bean.png',
+                width: 25,
+                colorBlendMode: BlendMode.multiply,
+                color: Colors.brown[100],
+              ),
+
+            const Expanded(child: SizedBox()),
+            StyledButton(
+              onPressed: increaseStrength,
+              child: const Text('+'),
+            ),
+          ]
+        ),
+        Row(
+          children: [
+            // const StyledBodyText('Sugar: '),
+            const SizedBox(
+              width: 120, // 👈 fixed width for label area
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: StyledBodyText('Sugar: '),
+              ),
+            ),
+
+            if (sugars == 0) 
+              const Text('No Sugars Please!', style: TextStyle(
+                color: Colors.red,
+                fontWeight: FontWeight.bold,
+                fontSize: 17
+              )),
+            
+            for (int i = 0; i < sugars; i++)
+              Image.asset('assets/img/sugar_cube.png',
+                width: 25,
+                colorBlendMode: BlendMode.multiply,
+                color: Colors.brown[100],
+              ),
+
+            const Expanded(child: SizedBox()),
+            StyledButton(
+              onPressed: increaseSugars,
+              child: const Text('+'),
+            ),
+          ]
+        ),
+      ]
+    );
+  }
+}
